16d50c76d7658ef8a5b6e2ad2882e6561e7cf58c
riceharvest <myappleiddarimaan@gmail.com>
2026-03-12 12:27:28 +0100

    feat(next-session): modernize for Next.js 16 and add web API support

--- packages/next-session/src/index.ts
+++ packages/next-session/src/index.ts
@@ -23,11 +23,11 @@ export default function nextSession<T extends SessionRecord = SessionRecord>(
   type TypedSession = Session<T>;
 
   function decorateSession(
-    req: IncomingMessage,
-    res: ServerResponse,
     session: TypedSession,
     sessionId: string,
-    now: number
+    now: number,
+    commitFn: () => Promise<void>,
+    destroyFn: () => Promise<void>
   ) {
     Object.defineProperties(session, {
       id: { value: sessionId, enumerable: true, writable: true },
@@ -41,34 +41,23 @@ export default function nextSession<T extends SessionRecord = SessionRecord>(
         enumerable: false,
       },
       destroy: {
-        value: function destroy() {
-          (this as any)[isDestroyed] = true;
-          delete req.session;
-          (this as any).cookie.maxAge = -1;
-          (this as any).cookie.expires = new Date(0);
-          commitHeader(res, name, this as any, encode);
-          return store.destroy((this as any).id);
-        },
+        value: destroyFn,
         enumerable: false,
       },
       commit: {
-        value: async function commit() {
-          await commitHeader(res, name, this as any, encode);
-          await store.set((this as any).id, this as any);
-        },
+        value: commitFn,
         enumerable: false,
       },
     });
   }
 
-  return async function getSession(
+  async function getSession(
     req: IncomingMessage,
     res: ServerResponse
   ): Promise<TypedSession> {
     if (req.session) return req.session as TypedSession;
 
     const _now = Date.now();
-
     const cookies = parse(req.headers?.cookie || "");
     const rawSid = cookies[name];
     const sessionId = rawSid ? (decode ? decode(rawSid) : rawSid) : null;
@@ -78,51 +67,52 @@ export default function nextSession<T extends SessionRecord = SessionRecord>(
     let session: TypedSession;
     if (_session) {
       session = _session as TypedSession;
-      // Some store return cookie.expires as string, convert it to Date
       if (typeof (session.cookie as any).expires === "string") {
         (session.cookie as any).expires = new Date((session.cookie as any).expires);
       }
-
-      // Add session methods
-      decorateSession(req, res, session, sessionId as string, _now);
-
-      // Extends the expiry of the session if options.touchAfter is satisfied
-      if (touchAfter >= 0 && (session.cookie as any).expires && (session.cookie as any).maxAge) {
-        const lastTouchedTime =
-          (session.cookie as any).expires.getTime() - (session.cookie as any).maxAge * 1000;
-        if (_now - lastTouchedTime >= touchAfter * 1000) session.touch();
-      }
     } else {
-      const newSessionId = genid();
       session = {
         cookie: {
           path: cookieOptions.path || "/",
-          httpOnly:
-            cookieOptions.httpOnly !== undefined
-              ? cookieOptions.httpOnly
-              : true,
+          httpOnly: cookieOptions.httpOnly !== undefined ? cookieOptions.httpOnly : true,
           domain: cookieOptions.domain,
           sameSite: cookieOptions.sameSite,
           secure: cookieOptions.secure || false,
           maxAge: cookieOptions.maxAge,
-          expires: cookieOptions.maxAge
-            ? new Date(_now + cookieOptions.maxAge * 1000)
-            : undefined,
+          expires: cookieOptions.maxAge ? new Date(_now + cookieOptions.maxAge * 1000) : undefined,
         },
       } as TypedSession;
       (session as any)[isNew] = true;
-      decorateSession(req, res, session, newSessionId, _now);
+    }
+
+    const currentSessionId = _session ? (sessionId as string) : genid();
+
+    const commitFn = async () => {
+      commitHeader(res, name, session, encode);
+      await store.set(session.id, session);
+    };
+
+    const destroyFn = async () => {
+      (session as any)[isDestroyed] = true;
+      delete req.session;
+      (session as any).cookie.maxAge = -1;
+      (session as any).cookie.expires = new Date(0);
+      commitHeader(res, name, session, encode);
+      return store.destroy(session.id);
+    };
+
+    decorateSession(session, currentSessionId, _now, commitFn, destroyFn);
+
+    if (_session && touchAfter >= 0 && (session.cookie as any).expires && (session.cookie as any).maxAge) {
+      const lastTouchedTime = (session.cookie as any).expires.getTime() - (session.cookie as any).maxAge * 1000;
+      if (_now - lastTouchedTime >= touchAfter * 1000) session.touch();
     }
 
     const prevHash = hash(session);
 
     if (autoCommit) {
       const _writeHead = res.writeHead;
-      res.writeHead = function resWriteHeadProxy(
-        this: ServerResponse,
-        statusCode: number,
-        ...args: any[]
-      ) {
+      res.writeHead = function resWriteHeadProxy(this: ServerResponse, statusCode: number, ...args: any[]) {
         if (!this.headersSent && (session[isTouched] || (session[isNew] && hash(session) !== prevHash))) {
           commitHeader(this, name, session, encode);
         }
@@ -145,9 +135,71 @@ export default function nextSession<T extends SessionRecord = SessionRecord>(
     }
 
     req.session = session;
+    return session;
+  }
+
+  async function getWebSession(
+    req: Request,
+    res: Response | Headers
+  ): Promise<TypedSession> {
+    const _now = Date.now();
+    const cookies = parse(req.headers.get("cookie") || "");
+    const rawSid = cookies[name];
+    const sessionId = rawSid ? (decode ? decode(rawSid) : rawSid) : null;
+
+    const _session = sessionId ? await store.get(sessionId) : null;
+
+    let session: TypedSession;
+    if (_session) {
+      session = _session as TypedSession;
+      if (typeof (session.cookie as any).expires === "string") {
+        (session.cookie as any).expires = new Date((session.cookie as any).expires);
+      }
+    } else {
+      session = {
+        cookie: {
+          path: cookieOptions.path || "/",
+          httpOnly: cookieOptions.httpOnly !== undefined ? cookieOptions.httpOnly : true,
+          domain: cookieOptions.domain,
+          sameSite: cookieOptions.sameSite,
+          secure: cookieOptions.secure || false,
+          maxAge: cookieOptions.maxAge,
+          expires: cookieOptions.maxAge ? new Date(_now + cookieOptions.maxAge * 1000) : undefined,
+        },
+      } as TypedSession;
+      (session as any)[isNew] = true;
+    }
+
+    const currentSessionId = _session ? (sessionId as string) : genid();
+    const headers = res instanceof Response ? res.headers : res;
+
+    const commitFn = async () => {
+      commitHeader(headers, name, session, encode);
+      await store.set(session.id, session);
+    };
+
+    const destroyFn = async () => {
+      (session as any)[isDestroyed] = true;
+      (session as any).cookie.maxAge = -1;
+      (session as any).cookie.expires = new Date(0);
+      commitHeader(headers, name, session, encode);
+      return store.destroy(session.id);
+    };
+
+    decorateSession(session, currentSessionId, _now, commitFn, destroyFn);
+
+    if (_session && touchAfter >= 0 && (session.cookie as any).expires && (session.cookie as any).maxAge) {
+      const lastTouchedTime = (session.cookie as any).expires.getTime() - (session.cookie as any).maxAge * 1000;
+      if (_now - lastTouchedTime >= touchAfter * 1000) session.touch();
+    }
+
+    // Web API doesn't support autoCommit because it relies on monkey-patching Node.js streams.
+    // Users of getWebSession must manually call session.commit() or handle headers.
 
     return session;
-  };
+  }
+
+  return Object.assign(getSession, { getWebSession });
 }
 
 export type { Options, SessionData, SessionStore, Session };
